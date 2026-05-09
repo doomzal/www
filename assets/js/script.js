4ebb6bea50ee71ddcbbdad54ed560eaa5825c216
domzal <64362366+doomzal@users.noreply.github.com>
2026-05-09 19:05:01 +0200

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-
-    if (window.scrollY > 50) {
-        navbar.style.background = 'rgba(5,8,22,.92)';
-    } else {
-        navbar.style.background = 'rgba(5,8,22,.7)';
-    }
-});
